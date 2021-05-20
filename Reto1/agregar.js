f8aaa9981b8f85537ee74e71fd7984ebcc764a4a
Agustinsn <agustinasn95@gmail.com>
2021-05-20 18:21:42 -0500

    Update agregar.js

--- Reto1/agregar.js
+++ Reto1/agregar.js
@@ -1,16 +1,29 @@
+const url = "https://60a4b6f2fbd48100179dcbe1.mockapi.io/lista";
+
 class ToDo{
     constructor(tarea){
         this.tarea =tarea;
     }
-    addTarea(tarea){
-        const formlist=document.getElementById('form')
-        const element=document.createElement('div');
-        element.innerHTML='<div class="row"><h1>${tarea}</h1></div>';
+    get imprimir(){
+        return this.juntar();
     }
-}
+    juntar(){
+        fetch(url,{
+            method:'POST',
+            body:JSON.stringify('lista'),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+        return this.tarea+"prueba";
+    }
+    }
+
 document.getElementById('btn').addEventListener('click',function(e){
     e.preventDefault();
     let tareaIngresada= document.getElementById("input").value;
-    const tarea =new ToDo(tareaIngresada);
-    addTarea(tarea);
+    const nuevaTarea= new ToDo(tareaIngresada)
+    console.log(nuevaTarea.imprimir)
 })
